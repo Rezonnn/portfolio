c2fe8dcef7884ac1428ae1d9ef6cc428f49190a7
Rezon Gautama <rezonhg@gmail.com>
2025-10-24 22:39:33 -0700

    Fix bugs

--- projects/projects.js
+++ projects/projects.js
@@ -2,10 +2,13 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 async function loadProjects() {
   const projects = await fetchJSON('../lib/projects.json');
-  const container = document.querySelector('.projects');
-  renderProjects(container, projects);
-
-  console.log(`Loaded ${projects.length} projects`);
+  if (Array.isArray(projects) && projects.length) {
+    const container = document.querySelector('.projects');
+    renderProjects(container, projects);
+    console.log(`Loaded ${projects.length} projects`);
+  } else {
+    console.error('No projects found or failed to load projects.json');
+  }
 }
 
 loadProjects();
