5a1a34aeb41720ebb9b061721a83acb40e10c997
Rezon Gautama <rezonhg@gmail.com>
2025-10-26 01:44:31 -0700

    Fixing Photos

--- global.js
+++ global.js
@@ -110,14 +110,16 @@ export async function fetchGitHubData(username) {
 }
 
 export function renderProjects(project, containerElement, headingLevel = 'h2') {
-  const base = document.body.dataset.base ?? '';
   containerElement.innerHTML = '';
 
+  const inProjectsDir =
+    /\/projects(\/|\/index\.html?)?$/.test(location.pathname);
+
   for (const p of project) {
-    
+
     const imgSrc = /^https?:\/\//.test(p.image)
       ? p.image
-      : (base + p.image).replace(/\/{2,}/g, '/');
+      : (inProjectsDir ? `../${p.image}` : p.image);
     
     const article = document.createElement('article');
     article.innerHTML = `
