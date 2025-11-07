6f4c27c6aec02a41f7eeca5dd92bbcf3a25bee80
Rezon Gautama <rezonhg@gmail.com>
2025-11-07 10:40:27 -0800

    Step 2

--- meta/main.js
+++ meta/main.js
@@ -1,5 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+// ---- Step 1.1: load CSV and convert types ----
 async function loadData() {
   const data = await d3.csv('loc.csv', row => ({
     ...row,
@@ -12,6 +13,7 @@ async function loadData() {
   return data;
 }
 
+// ---- Step 1.2: build commits array ----
 function processCommits(data) {
   return d3
     .groups(data, d => d.commit)
@@ -20,7 +22,7 @@ function processCommits(data) {
       const { author, date, time, timezone, datetime } = first;
       const ret = {
         id: commit,
-        url: 'https://github.com/YOUR_REPO/commit/' + commit,
+        url: 'https://github.com/YOUR_REPO/commit/' + commit, // optional
         author,
         date,
         time,
@@ -39,6 +41,7 @@ function processCommits(data) {
     });
 }
 
+// ---- Step 1.3: simple stats block ----
 function renderCommitInfo(data, commits) {
   const container = d3.select('#stats');
   const dl = container.append('dl').attr('class', 'stats');
@@ -54,7 +57,6 @@ function renderCommitInfo(data, commits) {
   const maxFileLen = d3.max(fileLengths, d => d[1]) ?? 0;
   const longestFile = (d3.greatest(fileLengths, d => d[1]) ?? [null])[0];
   const avgFileLen = d3.mean(fileLengths, d => d[1]) ?? 0;
-
   const avgDepth = d3.mean(data, d => d.depth) ?? 0;
 
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
@@ -79,6 +81,84 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(d3.format('.2f')(avgDepth));
 }
 
+// ---- Step 2: scatterplot (time of day vs date) ----
+function renderScatterPlot(_data, commits) {
+  const width = 1000;
+  const height = 600;
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  // Scales
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, d => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+  // Margins and usable area
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom
+  };
+
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Step 2.3: gridlines (draw BEFORE axes)
+  const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left},0)`);
+
+  gridlines.call(
+    d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width)
+  );
+
+  // Axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .attr('class', 'x-axis')
+    .call(xAxis);
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .attr('class', 'y-axis')
+    .call(yAxis);
+
+  // Dots
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', d => xScale(d.datetime))
+    .attr('cy', d => yScale(d.hourFrac))
+    .attr('r', 4)
+    .attr('fill', 'steelblue')
+    .attr('opacity', 0.85);
+}
+
+// ---- run everything ----
 const data = await loadData();
 const commits = processCommits(data);
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
