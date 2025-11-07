79c78923617913506a595216b447286219d24ad2
Rezon Gautama <rezonhg@gmail.com>
2025-11-07 09:49:29 -0800

    Meta

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ const pages = [
   { url: "projects/", title: "Projects" },
   { url: "resume/", title: "CV" },
   { url: "contact/", title: "Contact" },
+  { url: "meta/", title: "Meta" },
   { url: "https://github.com/Rezonnn", title: "GitHub" },
   { url: "https://www.linkedin.com/in/rezonhg", title: "LinkedIn" }
 ];
